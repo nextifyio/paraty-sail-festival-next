67bdce8f396ceff0d30146ed8e8f560462dd3e33
Carlos Meira <meira.bcc@gmail.com>
2025-09-09 21:20:13 -0300

    Coorecao config eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,11 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "@next/next/no-sync-scripts": "off"
+    }
   },
+  
 ];
 
 export default eslintConfig;
